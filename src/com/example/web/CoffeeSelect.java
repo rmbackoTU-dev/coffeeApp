9e26e52356422ce0181ad5c8119957ab55fc73e8
Ryan Backof <rmbacko92@gmail.com>
2022-11-06 15:27:26 -0500

    Updated servlet to coffeeSelect to differentiate from book

--- src/com/example/web/CoffeeSelect.java
+++ src/com/example/web/CoffeeSelect.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.http.HttpServlet;
 import java.io.*;
 
-public class BeerSelect extends HttpServlet{
+public class CoffeeSelect extends HttpServlet{
 
     public void doPost(HttpServletRequest request,
                     HttpServletResponse reponse)
@@ -14,8 +14,8 @@ public class BeerSelect extends HttpServlet{
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
 
-        out.println("Beer Selection Advice: <br>");
+        out.println("Coffee Selection Advice: <br>");
         String c=request.getParameter("color");
-        out.println("<br>Got beer color "+c);
+        out.println("<br>Got coffee color "+c);
     }
 }
