a395d7f7c5a7d03553bb954a0d440c378e9a2794
Ryan Backof <rmbacko92@gmail.com>
2022-11-08 06:55:01 -0500

    Updated example to work around coffee Scenario, tested remove and reinstall. Issue with ant reload still exists

--- src/com/example/web/CoffeeSelect.java
+++ src/com/example/web/CoffeeSelect.java
@@ -8,14 +8,14 @@ import java.io.*;
 public class CoffeeSelect extends HttpServlet{
 
     public void doPost(HttpServletRequest request,
-                    HttpServletResponse reponse)
+                    HttpServletResponse response)
                     throws IOException, ServletException
     {
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
 
         out.println("Coffee Selection Advice: <br>");
-        String c=request.getParameter("color");
-        out.println("<br>Got coffee color "+c);
+        String c=request.getParameter("roast");
+        out.println("<br>Got coffee roast type "+c);
     }
 }
